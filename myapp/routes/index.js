7ecadda93dd79b7557e0bbfe69dfeb9b53e4de6e
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-11-13 12:29:55 +0100

    sem 2 install sqlite3 + 3 couches vide

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/pizza', function(req, res, next) {
+    res.send('Bienvenue chez des pizzas');
+})
 module.exports = router;
