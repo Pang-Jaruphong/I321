5651d49c61c8076a23312c21c091b0861697c50f
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-12-04 09:32:50 +0100

    Ajouter la listes des ingrédients

---
+++ myapp/routes/ingredients.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const dbcon = require('../config/database');
+
+/* GET incredients listing. */
+router.get('/',  async function (req, res, next) {
+    try {
+        const sqlQuery = 'SELECT * FROM ingredients';
+        const [rows, fields] = await dbcon.execute(sqlQuery);
+        res.json(rows);
+    } catch (err) {
+        console.error("Erreur de BDD", err.message);
+        res.status(500).json({
+            message: 'Erreur serveur : Impossible de lire la base de données.',
+            error: err.message});
+    }
+});
+
+module.exports = router;
