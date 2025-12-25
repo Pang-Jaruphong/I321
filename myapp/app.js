7e31c9e7d9493d12f38ff622efba1bdbac5bbf42
pang-jaruphong <you@example.com>
2025-12-25 15:33:05 +0100

    Ajouter README et installer swagger

--- myapp/app.js
+++ myapp/app.js
@@ -11,6 +11,26 @@ var specialRouter = require('./routes/special');
 
 var app = express();
 
+// Aider par ChatGPT pour l'utilisation de swagger
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Pizza API Documentation',
+            version: '1.0.0',
+            description: 'Documentation interactive de mon API de Pizzas',
+        },
+        servers: [{ url: 'http://localhost:3002' }],
+    },
+    apis: ['./routes/*.js'], // Swagger va lire les commentaires dans tes fichiers routes
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
